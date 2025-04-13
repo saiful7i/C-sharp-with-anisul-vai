4670eafdfcdf9eae60048c9687daa4893fc6dda1
Md. Saiful Islam <saiful7i3301@gmail.com>
2025-04-13 13:32:59 +0600

    Create constant and multiple variable.cs

---
+++ Practice/constant and multiple variable.cs
@@ -0,0 +1,32 @@
+using System;
+class Program
+{
+    public static void Main(string[] args)
+    {
+        //constant
+        const string universityName = "Dhaka International University";
+        Console.WriteLine(universityName);
+
+        string highSchoolName = "Ucep Bangladesh";
+        Console.WriteLine(highSchoolName);
+
+        highSchoolName = "Ucep Ismail School";
+        Console.WriteLine(highSchoolName);
+
+        //multiple variables
+        int number1;
+        int number2;
+        int number3;
+
+        number1 = 10;
+        number2 = 10;
+        number3 = 10;
+        Console.WriteLine(number3);
+        number1 = number2 = number3 = 20;
+
+        Console.WriteLine(number1);
+        
+
+    }
+
+}
