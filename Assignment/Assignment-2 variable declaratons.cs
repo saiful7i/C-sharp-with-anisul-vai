50d9ffd463d2525777e0e98359506fb6f2344f65
Md. Saiful Islam <saiful7i3301@gmail.com>
2025-04-13 13:23:30 +0600

    Create Assignment-2 variable declaratons.cs

---
+++ Assignment/Assignment-2 variable declaratons.cs
@@ -0,0 +1,21 @@
+using System;
+class Program
+{
+    public static void Main(string[] args)
+    {
+        string produectName = "Apple iphone 14";
+        double productPrice = 320.5;
+        string productCategory = "smart phone";
+        bool availableStatus = true;
+        int productSold = 5;
+
+        Console.WriteLine("Name:"+produectName);
+        Console.WriteLine("Price: $" + productPrice);
+        Console.WriteLine("category:" +productCategory);
+        Console.WriteLine("Available:" + availableStatus);
+        Console.WriteLine("sold:" +productSold);
+        
+
+    }
+
+}
