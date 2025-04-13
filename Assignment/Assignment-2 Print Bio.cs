c99c75a393a2d57c173c488155edc55faaf266d8
Md. Saiful Islam <saiful7i3301@gmail.com>
2025-04-13 12:38:00 +0600

    Create Assignment-2 Print Bio.cs

---
+++ Assignment/Assignment-2 Print Bio.cs
@@ -0,0 +1,10 @@
+using System;
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("My Name is MD. Saiful Islam");
+        Console.WriteLine("I am 23 years Old");
+        Console.WriteLine("I am a full-stack Developer");
+    }
+}
