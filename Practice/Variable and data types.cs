c7d0cc9a50b529824b593bb6da6ffbc4fa0d6385
Md. Saiful Islam <saiful7i3301@gmail.com>
2025-04-13 13:03:59 +0600

    Create Variable and data types.cs

---
+++ Practice/Variable and data types.cs
@@ -0,0 +1,23 @@
+using System;
+class Program
+{
+    public static void Main(string[] args)
+    {
+        //Variable accept with letter,digit and _ . It's also case-sensitive.
+        //cannot start with digit and doesn't accept key word and space
+     
+     string name = "Md. Saiful Islam"; //name have string value.
+     int age = 23; // age have integer value.
+     double hight = 5.5; //hight have double value.
+     char group = 'B'; //group have character value.
+     bool student = true; // student have bool value.
+
+     Console.WriteLine(name);
+     Console.WriteLine(age);
+     Console.WriteLine(hight);
+     Console.WriteLine(group);
+     Console.WriteLine(student);
+
+    }
+
+}
