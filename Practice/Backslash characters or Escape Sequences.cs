6166d94c3e13a13b112a5bdfc50a015305d390dc
Md. Saiful Islam <saiful7i3301@gmail.com>
2025-04-13 12:46:18 +0600

    Create Backslash characters or Escape Sequences.cs

---
+++ Practice/Backslash characters or Escape Sequences.cs
@@ -0,0 +1,10 @@
+using System;
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("\"My Name is MD. Saiful Islam.\n");
+        Console.WriteLine("\t I am 23 years Old");
+        Console.WriteLine("I am a full-stack Developer");
+    }
+}
