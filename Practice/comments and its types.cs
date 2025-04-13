77e5e6f8e890f3a1fb060e4e29023aa2d83b77e9
Md. Saiful Islam <saiful7i3301@gmail.com>
2025-04-13 12:49:36 +0600

    Create comments and its types.cs

---
+++ Practice/comments and its types.cs
@@ -0,0 +1,14 @@
+using System;
+class Program
+{
+    public static void Main(string[] args)
+    {
+        // area of a triangle = 0.5 * base * heights
+        //0.5 * b * h //single line commment
+        Console.WriteLine("\"My Name is MD. Saiful Islam.\n");
+        Console.WriteLine("\t I am 23 years Old");
+        /*Console.WriteLine("I am a full-stack Developer");
+    */ //Multiline commments 
+    }
+
+}
