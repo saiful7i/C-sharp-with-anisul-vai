d507ccb47a6b1f0a27890a3119604b403208c4b0
Md. Saiful Islam <saiful7i3301@gmail.com>
2025-04-13 17:13:56 +0600

    Create 9mathematical operations.cs

---
+++ Practice/9mathematical operations.cs
@@ -0,0 +1,13 @@
+using System;
+class Program
+{
+    public static void Main(string[] args)
+    {
+        int result = 234 + 343;
+        Console.WriteLine(result);
+
+        Console.WriteLine(23 - 23 + 5);
+        Console.WriteLine(4*5+5);
+    }
+
+}
