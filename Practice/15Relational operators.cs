f107f6fc1837725eb0c397ec337a97c889447ba8
Md. Saiful Islam <saiful7i3301@gmail.com>
2025-04-15 12:35:25 +0600

    Create 15Relational operators.cs

---
+++ Practice/15Relational operators.cs
@@ -0,0 +1,20 @@
+using System;
+class Program
+{
+    public static void Main(string[] args)
+    {
+        //comparision operators -> ture/flase
+        int number1 = 30;
+        int number2 = 20;
+
+        Console.WriteLine(number1>number2);
+        Console.WriteLine(number1>=number2);
+        Console.WriteLine(number1<number2);
+        Console.WriteLine(number1<=number2);
+        Console.WriteLine(number1==number2);
+        Console.WriteLine(number1!=number2);
+        
+
+    }
+
+}
