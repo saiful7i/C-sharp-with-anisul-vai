a1e1db1b2407037aca4603bb0a4d25733541d07a
Md. Saiful Islam <saiful7i3301@gmail.com>
2025-04-19 18:25:23 +0600

    Create 30 for loop.cs

---
+++ Practice/30 for loop.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Threading.Channels;
+class Program
+{
+    
+    public static void Main(string[] args)
+    {
+        for (int n=1; n<=100; n++)
+        {
+            Console.Write($"{n} Bangladesh");
+
+        }
+
+    }
+
+}
