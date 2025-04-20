aca6080fc57a6bf9816a016da344d05d4f69d888
Md. Saiful Islam <saiful7i3301@gmail.com>
2025-04-20 17:52:36 +0600

    Update Project 3 | multiplication table from m to n.cs
    initial commit

--- Project/Project 3 | multiplication table from m to n.cs
+++ Project/Project 3 | multiplication table from m to n.cs
@@ -1,1 +1,18 @@
+using System;
+using System.Threading.Channels;
+class Program
+{
+   
+    public static void Main(string[] args)
+    {
+        Console.Write("Enter any number:"); 
+        int number = Convert.ToInt32(Console.ReadLine());
 
+        for (int i = 1; i <= 10; i++)
+        {
+            Console.WriteLine($"{number} * {i} = {number * i}");
+        }
+
+    }
+
+}
