49309121b293fb5810fbe4285657745317972004
Md. Saiful Islam <saiful7i3301@gmail.com>
2025-04-20 18:05:25 +0600

    Update Project 3 | multiplication table from m to n.cs

--- Project/Project 3 | multiplication table from m to n.cs
+++ Project/Project 3 | multiplication table from m to n.cs
@@ -5,12 +5,22 @@ class Program
    
     public static void Main(string[] args)
     {
-        Console.Write("Enter any number:"); 
-        int number = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Enter start number:"); 
+        int startNumber = Convert.ToInt32(Console.ReadLine());
 
-        for (int i = 1; i <= 10; i++)
+        Console.Write("Enter end number:");
+        int endNumber = Convert.ToInt32(Console.ReadLine());
+
+        for (int i = startNumber; i <= endNumber; i++)
         {
-            Console.WriteLine($"{number} * {i} = {number * i}");
+            Console.WriteLine($"Multiplication of {i}");
+            for (int j =1; j <= 10; j++)
+            {
+                Console.WriteLine($"{i} * {j} = {i*j}");
+            }
+            Console.WriteLine("---------------------");
+            
+            
         }
 
     }
