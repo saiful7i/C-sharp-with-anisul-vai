dab17b36553e8a8e92b90c7f308adcbcf4370a68
Md. Saiful Islam <saiful7i3301@gmail.com>
2025-04-20 12:53:53 +0600

    Create 31series.cs

---
+++ Practice/31series.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Threading.Channels;
+class Program
+{
+   
+    public static void Main(string[] args)
+    {
+        Console.Write("First number of series:");
+        int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Last number of series:");
+        int lastNumber = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Difference between number of series:");
+        int differenceNumber = Convert.ToInt32(Console.ReadLine());
+
+        for (int n=firstNumber; n<=lastNumber; n=n+differenceNumber)
+        {
+            Console.Write($"{n} ");
+
+        }
+
+    }
+
+}
