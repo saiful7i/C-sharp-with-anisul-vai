ca677cea1aa86aece9905d8b3f7a7ef736c5dba2
Md. Saiful Islam <saiful7i3301@gmail.com>
2025-04-21 16:54:02 +0600

    Create 41OOP | Constructor.cs

---
+++ Practice/41OOP | Constructor.cs
@@ -0,0 +1,44 @@
+using System;
+
+class Person
+{
+    public string name;
+    public int age;
+
+    public Person()
+    {
+        name = "test";
+        age = 0;
+    
+    }
+
+    public Person(string n, int a)
+    {
+        name = n;
+        age = a;
+
+    }
+
+    public void DisplayInfo()
+    {
+        Console.WriteLine($"Person Name: {name} and age: {age}");
+
+    }
+}
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Person p1 = new Person("Md.Saiful Islam", 23);
+        p1.DisplayInfo();
+
+        Person p2 = new Person("Anisul Islam", 33);
+        p2.DisplayInfo();
+
+        Person p3 = new Person();
+        p3.DisplayInfo();
+
+
+    }
+
+}
