2fbffbf640591721bb585de0ba8be9ffcf96154c
Md. Saiful Islam <saiful7i3301@gmail.com>
2025-04-24 23:43:41 +0600

    Create 48array.cs

---
+++ Practice/48array.cs
@@ -0,0 +1,11 @@
+class Program
+{
+  public static void Main(string[] args)
+  {
+    string[] names = { "anisul", "nusrat","alex"};
+    Console.WriteLine($"{names[0]}");
+    Console.WriteLine($"{names[1]}");
+    Console.WriteLine($"{names[2]}");    
+  }
+  
+}
