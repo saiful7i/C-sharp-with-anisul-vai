d53d9aeb24337af415f5ade61189f8b7fab72cb1
Md. Saiful Islam <saiful7i3301@gmail.com>
2025-04-24 12:53:30 +0600

    Update 47Exception Handling.cs using while loop for exception

--- Practice/47Exception Handling.cs
+++ Practice/47Exception Handling.cs
@@ -12,19 +12,6 @@ public class Program
         double result = num1 / num2;
         Console.WriteLine($"{result}");
       }
-      catch (ArgumentNullException)
-      {
-        Console.WriteLine($"Error: Input can not be null.");
-        
-      }
-      catch (OverflowException)
-      {
-        Console.WriteLine($"Number was too big or small for Int32");
-      }
-      catch (FormatException)
-      {
-        Console.WriteLine($"Invalid Input! Please Enter a valid integer");
-      }
       catch(DivideByZeroException){
         Console.WriteLine($"Error: Can not divide by zero");        
       }
@@ -46,21 +33,30 @@ public class Program
     {
         while (true)
         {
-          Console.Write($"Enter {prompt} = ");
-          string input = Console.ReadLine() ?? "";
-          if (string.IsNullOrEmpty(input))   
-          {
-            Console.WriteLine("num1 can not be null or empty");
-            continue;
-          }
           try
           {
+            Console.Write($"Enter {prompt} = ");
+            string input = Console.ReadLine() ?? "";
+            if (string.IsNullOrEmpty(input))   
+            {
+              Console.WriteLine($"{prompt} can not be null or empty");
+              continue; 
+            }
             return Convert.ToInt32(input);
           }
-          catch(FormatException)
+          catch (ArgumentNullException)
           {
-            Console.WriteLine($"Invalid Input ! Please enter a valid integer");
+            Console.WriteLine($"Error: Input can not be null.");
           }
+          catch (OverflowException)
+          {
+            Console.WriteLine($"Number was too big or small for Int32");
+          }
+          catch (FormatException)
+          {
+            Console.WriteLine($"Invalid Input! Please Enter a valid integer");
+          }          
         }
     }
 }
+
