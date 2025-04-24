25d1f1ab9dfad401c893152ff0445d614c9c6dc9
Md. Saiful Islam <saiful7i3301@gmail.com>
2025-04-24 15:23:03 +0600

    Create Project 4 | student management project part-1.cs

---
+++ Project/Project 4 | student management project part-1.cs
@@ -0,0 +1,71 @@
+using System;
+
+class  Student
+ {
+  //Private Fields. for name,dateOfBirth & Roll
+  private string? _name; //string? start with assign null for Non-nullable Property.
+  private DateTime _dateOfBirth; 
+  private string? _roll;
+
+  //Properties to access in Private filed 
+  public string Name{get; private set;}
+  public DateTime DateOfBirth{get;private set;}
+  public string Roll{get;private set;}
+
+  //constructor to assign value for private field
+  public Student(string name,DateTime dateOfBirth, string roll){
+    if(dateOfBirth > DateTime.Now){
+          throw new ArgumentException("Date of birth can not be in the future");
+        }
+    Name = name;
+    DateOfBirth = dateOfBirth;
+    Roll = roll;
+  }
+
+  private int CalculateAge(){
+    int age = DateTime.Now.Year - DateOfBirth.Year;
+    //check if the birthday for this year has occurred yet
+    if(DateTime.Now<DateOfBirth.AddYears(age)){
+      //if the birthday hasn't occurred yet, decrement the age by 1
+      age--;
+    }
+    return age;
+  }
+
+  public int Age{
+    get{return CalculateAge(); }  
+  }
+
+  public void StudentInformation(){
+     Console.WriteLine($"Roll:{Roll}\t Name:{Name}\t Date of Birth:{DateOfBirth.ToShortDateString()} \t Age:{Age}");
+  }
+  
+}
+ class Program
+ {
+  public static void Main(string[] args)
+  {
+    try
+    {
+      Student student1 = new Student(
+        "Md.Saiful Islam",new DateTime(2001,07,23),"CSE101");
+      Student student2 = new Student(
+        "Anisul Islam", new DateTime(2000,3,20),"CSE102");
+      //Display Info
+      Console.WriteLine($"====================================Student Information===================================");
+      student1.StudentInformation();
+      student2.StudentInformation();
+      
+         
+    }
+    catch (Exception e)
+    {
+      Console.WriteLine($"Error:{e.Message}");
+      
+    }
+    
+    
+  }
+ }
+
+ 
