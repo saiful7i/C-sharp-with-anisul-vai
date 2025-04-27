b32d7fedaf93a9a614847679b469b37028fdcf08
Md. Saiful Islam <saiful7i3301@gmail.com>
2025-04-28 00:23:52 +0600

    Update and rename shallow copy vs deep copy.cs to 57shallow copy vs deep copy.cs

--- Practice/57shallow copy vs deep copy.cs
+++ Practice/57shallow copy vs deep copy.cs
@@ -16,7 +16,24 @@ public class Program
 
     Console.WriteLine($"Shallow Copy of Array after modification:");
     PrintArray(shallowCopy);
-    
+
+    Console.WriteLine("===========================================");
+
+    int[] originalArray2 = { 1,2,3,4,5,6,7};
+    int[] deepCopy = new int[originalArray2.Length];
+    Array.Copy(originalArray2, deepCopy,originalArray2.Length);//To copy Array. Array.Copy(copy form,destination,length)
+    Console.WriteLine($"Original Array before modification:");
+    PrintArray(originalArray2);
+
+    Console.WriteLine($"Shallow Copy of Array before modification:");
+    PrintArray(deepCopy);
+
+    deepCopy[0] = 10;
+    Console.WriteLine($"Original Array before modification:");
+    PrintArray(originalArray2);
+
+    Console.WriteLine($"Shallow Copy of Array before modification:");
+    PrintArray(deepCopy);
 
   }
   public static void PrintArray(int[] array){
@@ -24,6 +41,5 @@ public class Program
       Console.Write($"{i} ");      
     }
     Console.WriteLine("");
-
   }
 }
