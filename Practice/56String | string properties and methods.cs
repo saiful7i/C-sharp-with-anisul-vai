0a90742f36f551bedc7ef6d168b65770f651161e
Md. Saiful Islam <saiful7i3301@gmail.com>
2025-04-27 17:32:38 +0600

    Update 56String | string properties and methods.cs

--- Practice/56String | string properties and methods.cs
+++ Practice/56String | string properties and methods.cs
@@ -2,13 +2,31 @@ public class Program
 {
   public static void Main(string[] args)
   {
-    string text = "Hello everyone ! This is Saiful Islam";
+    string text = "Hello everyone ! This is Saiful";
     Console.WriteLine(text);
     Console.WriteLine($"Length of text: {text.Length}");
     Console.WriteLine($"0 index of text: {text[0]}");
+    
     bool isEmpty = string.IsNullOrEmpty(text);
     Console.WriteLine($"Is string empty or null: {isEmpty}");
- 
+
+    string insertString = text.Insert(0,"Hi!");
+    Console.WriteLine($"String Insert: {insertString}");
+
+    string removeString = text.Remove(2);
+    Console.WriteLine($"after Remove String: {removeString}");
     
+    
+    string textUpper = text.ToUpper();
+    Console.WriteLine($"String with Upper Case: {textUpper}");
+
+    string textLower = text.ToLower();
+    Console.WriteLine($"String with Lower Case: {textLower}"); 
+
+    string trimmedString = text.Trim(); //TrimStart(), TrimEnd()
+    Console.WriteLine($"String with Lower Case: {trimmedString}"); 
+
+    string substring = text.Substring(6,15); //TrimStart(), TrimEnd()
+    Console.WriteLine($"String with Lower Case: {substring}");        
   }
 }
