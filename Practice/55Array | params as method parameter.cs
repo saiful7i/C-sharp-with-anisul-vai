ff8ad541e2c6b1dd87206221b839c02b8ee62514
Md. Saiful Islam <saiful7i3301@gmail.com>
2025-04-27 17:08:07 +0600

    Create 55Array | params as method parameter.cs

---
+++ Practice/55Array | params as method parameter.cs
@@ -0,0 +1,11 @@
+public class Program
+{
+  public static int Sum(int num1,int num2){
+    return num1 + num2;
+  }
+  public static void Main(string[] args)
+  {
+    Console.WriteLine($"{Sum(5,4)}");
+    
+  }
+}
