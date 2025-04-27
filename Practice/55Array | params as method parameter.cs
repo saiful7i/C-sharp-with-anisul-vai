9c25e3c928d7c2d71c455d9ba71d5b0ef98a21c4
Md. Saiful Islam <saiful7i3301@gmail.com>
2025-04-27 17:11:50 +0600

    Update 55Array | params as method parameter.cs

--- Practice/55Array | params as method parameter.cs
+++ Practice/55Array | params as method parameter.cs
@@ -1,11 +1,18 @@
 public class Program
 {
-  public static int Sum(int num1,int num2){
-    return num1 + num2;
+  public static int Sum(params int[] number){ //params key word to use multiple parameters
+    int sum = 0;
+    foreach(int i in number){
+      sum += i;
+    }
+    return sum;
   }
   public static void Main(string[] args)
   {
     Console.WriteLine($"{Sum(5,4)}");
+    Console.WriteLine($"{Sum(5,4,3)}");
+    Console.WriteLine($"{Sum(5,4,5,7)}");
+    Console.WriteLine($"{Sum(5,4,3,1,4)}");
     
   }
 }
