5af0624261b0c7bac4bd2914088aca8b9b91e258
Md. Saiful Islam <saiful7i3301@gmail.com>
2025-04-27 17:43:32 +0600

    Update 56String | string properties and methods.cs

--- Practice/56String | string properties and methods.cs
+++ Practice/56String | string properties and methods.cs
@@ -2,7 +2,7 @@ public class Program
 {
   public static void Main(string[] args)
   {
-    string text = "Hello everyone ! This is Saiful";
+    string text = "Hello everyone! This is Saiful";
     Console.WriteLine(text);
     Console.WriteLine($"Length of text: {text.Length}");
     Console.WriteLine($"0 index of text: {text[0]}");
@@ -10,11 +10,18 @@ public class Program
     bool isEmpty = string.IsNullOrEmpty(text);
     Console.WriteLine($"Is string empty or null: {isEmpty}");
 
-    string insertString = text.Insert(0,"Hi!");
-    Console.WriteLine($"String Insert: {insertString}");
+    // string insertString = text.Insert(0,"Hi!");
+    // Console.WriteLine($"String Insert: {insertString}");
 
-    string removeString = text.Remove(2);
-    Console.WriteLine($"after Remove String: {removeString}");
+    // string removeString = text.Remove(2);
+    // Console.WriteLine($"after Remove String: {removeString}");
+
+    string replaceString = text.Replace("Hello everyone!","Hi!");
+    Console.WriteLine($"String Insert: {replaceString}");
+
+    bool isExist = text.Contains("Saiful");
+    Console.WriteLine($"Saiful Is exit in the text:{isExist}");
+    
     
     
     string textUpper = text.ToUpper();
@@ -27,6 +34,6 @@ public class Program
     Console.WriteLine($"String with Lower Case: {trimmedString}"); 
 
     string substring = text.Substring(6,15); //TrimStart(), TrimEnd()
-    Console.WriteLine($"String with Lower Case: {substring}");        
+    Console.WriteLine($"substring of the text: {substring}");        
   }
 }
