9fa91be036599ca2725c9e8fbe07948ca0617a43
Md. Saiful Islam <saiful7i3301@gmail.com>
2025-04-28 16:17:13 +0600

    Update 59LINQ | select and selectMany operator.cs

--- Practice/59LINQ | select and selectMany operator.cs
+++ Practice/59LINQ | select and selectMany operator.cs
@@ -1,23 +1,53 @@
+using System.Diagnostics;
+
 public class Person{
   public string? Name{ get; set; }
   public int Age{ get; set; }
+  public string? BloodGroup{ get; set; }
 }
 
 class Program
 {
-  public static void Main(string[] args)
+  public static void Main(string[] args){
+  Stopwatch sw = new Stopwatch();
+    sw.Start();
   {
     int[] numbers = {1,2,3,4,5,6};
-
     var squareNumbers=numbers.Select(num => num*num);
-
     foreach(int item in squareNumbers){
       Console.Write($"{item} ");      
     }
     Console.WriteLine("");
-
-    
-  
   }
+  List<Person> people = new List<Person>{
+    new Person {Name = "Saiful",Age = 23, BloodGroup= "A+"},
+    new Person {Name = "Anisul",Age= 33, BloodGroup= "B-"},
+    new Person {Name = "Ovi",Age=34, BloodGroup= "O+"},
+    new Person {Name = "Alia",Age=18, BloodGroup= "AB+"},
+    new Person {Name = "Nusrat",Age=42, BloodGroup= "A-"},
+  };
 
+  var Name= people.Select(person => (person.Name, person.BloodGroup));
+  foreach(var item in Name){
+    Console.WriteLine(item.Name+" ="+item.BloodGroup);
+  }
+  Console.WriteLine("");
+
+  List<List<int>> nestedList = new List<List<int>>{
+    new List<int>{1,2,3,4,5},
+    new List<int>{4,5,6},
+    new List<int>{7,8},
+    new List<int>{9},
+  };
+
+  var flattedList = nestedList.SelectMany(list=> list);
+  foreach(var item in flattedList){
+    Console.Write(item+" ");
+  }
+  Console.WriteLine("");
+  
+  sw.Stop();
+  Console.WriteLine($"Time:{sw.ElapsedMilliseconds}ms");
+  
+}
 }
