b4bad162f5d890ec7c5bd5234a23fc5277645427
Md. Saiful Islam <saiful7i3301@gmail.com>
2025-04-29 22:59:27 +0600

    Update 59LINQ | select and selectMany operator.cs

--- Practice/59LINQ | select and selectMany operator.cs
+++ Practice/59LINQ | select and selectMany operator.cs
@@ -5,7 +5,6 @@ public class Person{
   public int Age{ get; set; }
   public string? BloodGroup{ get; set; }
 }
-
 class Program
 {
   public static void Main(string[] args){
@@ -26,28 +25,23 @@ class Program
     new Person {Name = "Alia",Age=18, BloodGroup= "AB+"},
     new Person {Name = "Nusrat",Age=42, BloodGroup= "A-"},
   };
-
   var Name= people.Select(person => (person.Name, person.BloodGroup));
   foreach(var item in Name){
     Console.WriteLine(item.Name+" ="+item.BloodGroup);
   }
   Console.WriteLine("");
-
   List<List<int>> nestedList = new List<List<int>>{
     new List<int>{1,2,3,4,5},
     new List<int>{4,5,6},
     new List<int>{7,8},
     new List<int>{9},
-  };
-
+  }
   var flattedList = nestedList.SelectMany(list=> list);
   foreach(var item in flattedList){
     Console.Write(item+" ");
   }
   Console.WriteLine("");
-  
   sw.Stop();
   Console.WriteLine($"Time:{sw.ElapsedMilliseconds}ms");
-  
 }
 }
