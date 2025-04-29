f42367c6f9a23eae1edf75795be710118b2d4b1b
Md. Saiful Islam <saiful7i3301@gmail.com>
2025-04-29 23:40:28 +0600

    Update 60LINQ | sorting operators - orderby & thenby.cs

--- Practice/60LINQ | sorting operators - orderby & thenby.cs
+++ Practice/60LINQ | sorting operators - orderby & thenby.cs
@@ -27,21 +27,21 @@ public class Program
       Console.WriteLine($"{item.Name} : {item.Age}");
     }
 
-    // int[] numbers = { 4,6,7,1,9,2};
-    // Console.WriteLine($"Order By Ascending");
-    // var sortedNumbersAscending= numbers.OrderBy(num => num);
+    int[] numbers = { 4,6,7,1,9,2};
+    Console.WriteLine($"Order By Ascending");
+    var sortedNumbersAscending= numbers.OrderBy(num => num);
     
-    // foreach (var item in sortedNumbersAscending)
-    // {
-    //   Console.Write($"{item} ");
-    // }
-    // Console.WriteLine("");
+    foreach (var item in sortedNumbersAscending)
+    {
+      Console.Write($"{item} ");
+    }
+    Console.WriteLine("");
 
-    // Console.WriteLine($"Order By Descending");
-    // var sortedNumbersDescending= numbers.OrderByDescending(num => num);
-    // foreach(var item in sortedNumbersDescending){
-    // Console.Write($"{item} ");
-    // }
+    Console.WriteLine($"Order By Descending");
+    var sortedNumbersDescending= numbers.OrderByDescending(num => num);
+    foreach(var item in sortedNumbersDescending){
+    Console.Write($"{item} ");
+    }
   }
 }
 
